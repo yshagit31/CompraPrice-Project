b5acc7c88e7860bb07bed238bc2c3dfbdec4caf3
yshagit31 <yshashank024@gmail.com>
2025-07-09 11:23:42 +0530

    Amazon done

--- server/index.js
+++ server/index.js
@@ -1,77 +1,3 @@
-// const puppeteer = require('puppeteer');
-
-// (async () => {
-//   // Launch a new browser instance
-//   const browser = await puppeteer.launch({
-//     headless: false,  // set to false so you can see the browser open
-//     defaultViewport: null // opens in full screen
-//   });
-
-//   // Open a new page
-//   const page = await browser.newPage();
-
-//   // Navigate to the desired website
-// //   await page.goto('https://www.amazon.in/');
-//     await page.goto('https://www.amazon.in/s?k=s24', { waitUntil: 'domcontentloaded' });
-
-//   await page.waitForSelector('.s-result-item');
-
-//   const items = await page.$$('.s-result-item');
-
-
-
-//   for (let i = 0; i < items.length; i++) {
-//     const item = items[i];
-
-//     try {
-//       const symbol = await item.$eval('.a-price-symbol', el => el.innerText);
-//       // const whole = await item.$eval('.a-price-whole', el => el.innerText);
-//       const whole = await item.$eval('.a-price-whole', el => el.innerText.replace(/[^0-9]/g, ''));
-//         const priceInt = parseInt(whole);
-//       if (isNaN(priceInt)) {
-//         console.log(`Item ${i + 1}: Invalid price, skipping.`);
-//         continue;
-//       }
-
-//       const img=await item.$eval('.s-image',el=>el.alt)
-//       const imgData = await item.$eval('.s-image', el => ({
-//         alt: el.alt || '',
-//         src: el.src || ''
-//         }));
-//         const title = await item.$eval(
-//           '.s-title-instructions-style .a-size-base-plus span',
-//           el => el.innerText.trim()
-//         );
-
-//       const link = await item.$eval(
-//       '[data-component-type="s-product-image"] a',
-//       (el) => el.href
-//       );
-
-//         const results = [];
-
-//          results.push({
-//         title,
-//         price: whole,
-//         currency: symbol,
-//         image: imgData,
-//         link
-//       });
-
-
-//       // console.log(`Result is ${results} `);
-//       console.log('Result is:', results);
-
-//       // console.log(`Item ${i + 1}: Price = ${symbol}${whole} : Title :${link} `);
-//     } catch (err) {
-//       console.log(`Item ${i + 1}: Price not found`);
-//     }
-//   }
-
-// })();
-
-
-
 const puppeteer = require('puppeteer');
 
 (async () => {
@@ -81,7 +7,12 @@ const puppeteer = require('puppeteer');
   });
 
   const page = await browser.newPage();
-  await page.goto('https://www.amazon.in/s?k=s24+', { waitUntil: 'domcontentloaded' });
+
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/114.0.0.0 Safari/537.36');
+
+
+  await page.goto('https://www.amazon.in/s?k=s24+', { waitUntil: 'networkidle2' });
+
   await page.waitForSelector('.s-result-item');
 
   const results = await page.evaluate(() => {
